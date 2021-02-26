068dc33f7c8e8ed43b2699eb38507bfe6b7e76d9
Daniel Salgado <danielsalgado@unitec.edu>
2021-02-26 14:46:57 -0600

    Update database.php file

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'b00144a9eba68b'),
             'password' => env('DB_PASSWORD', '5b2c76a6'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
